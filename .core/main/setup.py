8af89809ceaa74c3a0d811a8ed16c851dd4d9646
SAYDOG OFFICIAL <66870420+saydog-official@users.noreply.github.com>
2020-08-18 15:32:28 +0700

    Add files via upload

---
+++ .core/main/setup.py
@@ -0,0 +1,17 @@
+import os,sys,time,fileinput
+def rename():
+	cek = open("../usr/bin/apktool241").read()
+	if "jarfile=apktool.jar" in cek:
+		pass
+	else:
+		pass
+	replaces = {"jarfile=apktool.jar":"jarfile=apktool241.jar"}
+	for line in fileinput.input("../usr/bin/apktool241", inplace=True):
+		for search in replaces:
+			replaced = replaces[search]
+			line = line.replace(search,replaced)
+		print(line, end="")
+		pass
+	sys.exit(1)
+
+rename()
