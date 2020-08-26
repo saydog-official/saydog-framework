7226442ae7963d205d51c5383f151b3b2531ae75
SAYDOG OFFICIAL <66870420+saydog-official@users.noreply.github.com>
2020-08-26 18:52:17 +0700

    update config_apktool241.py

--- .core/main/config_apktool241.py
+++ .core/main/config_apktool241.py
@@ -1,12 +1,12 @@
 import os,sys,time,fileinput
 def rename():
-	cek = open("~/../usr/bin/apktool241").read()
+	cek = open("/data/data/com.termux/files/usr/bin/apktool241").read()
 	if "jarfile=apktool.jar" in cek:
 		pass
 	else:
 		sys.exit(1)
 	replaces = {"jarfile=apktool.jar":"jarfile=apktool241.jar"}
-	for line in fileinput.input("~/../usr/bin/apktool241", inplace=True):
+	for line in fileinput.input("/data/data/com.termux/files/usr/bin/apktool241", inplace=True):
 		for search in replaces:
 			replaced = replaces[search]
 			line = line.replace(search,replaced)
